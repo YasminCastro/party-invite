450f8d8d0790fe6bbe657a6d9516f5fdea20eeb1
Yasmin Castro <yasminsdcastro@gmail.com>
2023-06-22 20:38:44 -0300

    feat: favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
