55cbd63eb7eaed918ad09016d2601e2cf3af9123
Yasmin Castro <yasminsdcastro@gmail.com>
2023-06-22 23:58:07 -0300

    feat: delete guest

---
+++ src/pages/api/delete-guest.ts
@@ -0,0 +1,25 @@
+import db from "@/lib/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function NewGuest(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { name } = req.body;
+
+    if (!name) throw new Error("Name is missing");
+
+    const database = await db;
+
+    if (!database) throw new Error("Database is not connected");
+
+    const collection = db.collection("guests");
+
+    const result = await collection.deleteOne({ name });
+    res.status(200).json(result);
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+}
