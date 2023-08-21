80abbd29fd80610588c7bc34661ab7d382d9cb01
Yasmin Castro <yasminsdcastro@gmail.com>
2023-08-21 19:46:49 -0300

    refactor; next congfi

--- next.config.js
+++ next.config.js
@@ -3,8 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     MONGO_URL: process.env.MONGO_URL,
+    DB_NAME: process.env.DB_NAME,
     SECRET_TOKEN: process.env.SECRET_TOKEN,
     GUESTS_PASSWORD: process.env.GUESTS_PASSWORD,
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
   },
 };
 
