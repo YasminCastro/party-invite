0e8e03ad5095542c50f9e1d2b3c74be458b9d9ac
Yasmin Castro <yasminsdcastro@gmail.com>
2023-08-22 10:56:41 -0300

    feat: confirmed

--- src/components/Cards/GoingCard/index.tsx
+++ src/components/Cards/GoingCard/index.tsx
@@ -1,6 +1,7 @@
 import SpotifyPlaylist from "@/components/SpotifyPlaylist";
 import Image from "next/image";
 import graphic from "/public/confirm/graphic-design-is-my-passion-designer.gif";
+import { Button } from "flowbite-react";
 
 import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
@@ -12,22 +13,21 @@ export default function GoingCard() {
   return (
     <>
       <NextSeo
-        title="404 • Niver da Yas"
+        title={`${projectConfig.seoName}`}
         description="Presença confirmada ou não..."
       />
 
       <div className="flex min-h-screen items-center justify-evenly bg-home bg-cover">
-        <button
-          className="absolute right-6 top-6 h-8 w-5 text-base"
+        <Button
+          className="absolute right-6 top-6"
+          gradientDuoTone="purpleToBlue"
           onClick={() => router.push("/")}
         >
           Voltar
-        </button>
+        </Button>
         <div className="flex gap-4 rounded-lg bg-gray-950 bg-opacity-80 p-8 max-md:w-full max-md:flex-col">
           <div className="flex flex-col max-md:mt-10">
-            <h2 className="font-bebas text-4xl text-yellow-400">
-              Aiiii mt bão você vai!!
-            </h2>
+            <h2 className="font-bebas text-4xl text-blue-400">!!</h2>
             <ul className="ml-4 w-[90%] list-disc font-bebas text-white max-md:w-full">
               <li className="text-xl">
                 Não se esqueça de levar sua bebida e caixa/bolsa térmica!!
@@ -40,7 +40,7 @@ export default function GoingCard() {
                   <a
                     href={`https://api.whatsapp.com/send?phone=${projectConfig.whatsappNumber}`}
                     target="_blank"
-                    className="font-mono text-yellow-300"
+                    className="font-mono text-blue-400"
                   >
                     zap
                   </a>{" "}
@@ -58,7 +58,7 @@ export default function GoingCard() {
 
               <li className="text-lg">
                 Quer me dar um presente e não sabe o que? pode fazer um pix para{" "}
-                <a className="font-mono text-yellow-300">
+                <a className="font-mono text-blue-400">
                   yasminsdcastro@gmail.com
                 </a>
               </li>
@@ -67,11 +67,11 @@ export default function GoingCard() {
                 <a
                   href={`https://api.whatsapp.com/send?phone=${projectConfig.whatsappNumber}&text=Oiee%2C%20quero%20convidar%20uma%20pessoa%20para%20seu%20niver...`}
                   target="_blank"
-                  className="font-mono text-yellow-300"
+                  className="font-mono text-blue-400"
                 >
                   aqui
                 </a>{" "}
-                para mandar um zap pra Yas pra ver se pode.
+                para mandar um zap pra ver se pode.
                 <p className="mt-[-5px] font-mono text-xs">
                   sujeito a lotação do local.
                 </p>
