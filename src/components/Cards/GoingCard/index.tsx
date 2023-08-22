6d3403d0e55565387ef24e802991ac4cbf4eb8c0
Yasmin Castro <yasminsdcastro@gmail.com>
2023-08-22 12:11:45 -0300

    refactor: presença confirmada

--- src/components/Cards/GoingCard/index.tsx
+++ src/components/Cards/GoingCard/index.tsx
@@ -18,16 +18,18 @@ export default function GoingCard() {
         <PurpleToBlueButton path="/" title="Voltar" />
 
         <div className="flex gap-4 rounded-lg bg-gray-950 bg-opacity-80 p-8 max-md:w-full max-md:flex-col">
-          <div className="flex flex-col max-md:mt-10">
-            <h2 className="font-bebas text-4xl text-blue-400">!!</h2>
+          <div className="flex flex-col ">
+            <h2 className="mb-4 font-bebas text-4xl text-blue-400">
+              Presença confirmada!
+            </h2>
             <ul className="ml-4 w-[90%] list-disc font-bebas text-white max-md:w-full">
-              <li className="text-xl">
+              <li className="text-2xl max-sm:text-xl">
                 Não se esqueça de levar sua bebida e caixa/bolsa térmica!!
               </li>
-              <li className="text-xl">
+              <li className="text-2xl max-sm:text-xl">
                 Caso você tenha conjuge não esqueça de confimar a presença delu
                 também!!
-                <p className="mt-[-5px] font-mono text-xs">
+                <p className="mt-[-5px] font-mono text-sm">
                   caso seu conjugue não esteja na lista me avisa no{" "}
                   <a
                     href={`https://api.whatsapp.com/send?phone=${projectConfig.whatsappNumber}`}
@@ -38,33 +40,26 @@ export default function GoingCard() {
                   </a>{" "}
                 </p>
               </li>
-              <div className="p-4 max-md:w-full">
-                <Image
-                  src={graphic}
-                  alt="Graphic design is my passion"
-                  sizes="(min-width: 60em) 24vw,
-          (min-width: 28em) 45vw,
-          100vw"
-                />
-              </div>
 
-              <li className="text-lg">
-                Quer me dar um presente e não sabe o que? pode fazer um pix para{" "}
-                <a className="font-mono text-blue-400">
-                  yasminsdcastro@gmail.com
-                </a>
+              <li className="text-2xl max-sm:text-xl">
+                Texto sobre a comida...
               </li>
-              <li className="text-lg">
+
+              <li className="text-2xl max-sm:text-xl">
+                Quer me dar um presente e não sabe o que? pode me dar{" "}
+                <a className=" text-blue-400">cerveja</a>
+              </li>
+              <li className="text-2xl max-sm:text-xl">
                 Quer levar alguem? clica{" "}
                 <a
                   href={`https://api.whatsapp.com/send?phone=${projectConfig.whatsappNumber}&text=Oiee%2C%20quero%20convidar%20uma%20pessoa%20para%20seu%20niver...`}
                   target="_blank"
-                  className="font-mono text-blue-400"
+                  className=" text-blue-400"
                 >
                   aqui
                 </a>{" "}
                 para mandar um zap pra ver se pode.
-                <p className="mt-[-5px] font-mono text-xs">
+                <p className="mt-[-5px] font-mono text-sm">
                   sujeito a lotação do local.
                 </p>
               </li>
