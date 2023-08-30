460e29f385ffac4441e4754bc192783e7fe88a73
Yasmin Castro <yasminsdcastro@gmail.com>
2023-08-30 09:30:35 -0300

    refactor: rhary number

--- src/config/project.ts
+++ src/config/project.ts
@@ -17,7 +17,7 @@ const projectConfig: IProject = {
   // spotifyUrl:
   //   "https://open.spotify.com/playlist/0Fyoivhuile8c1RSOEGdYd?si=91fa4819a3ee4eb6",
   spotifyUrl: null,
-  whatsappNumber: "5562981695581",
+  whatsappNumber: "5562981195060",
   address:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3821.5681504789345!2d-49.22603822518672!3d-16.698480084075918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef1b64b07dd33%3A0x89d093940492476a!2sLive%20Tower%20Lozandes!5e0!3m2!1spt-BR!2sbr!4v1687348514021!5m2!1spt-BR!2sbr",
 };
