6f140796706dee18b10c2b080775fc2b7568203c
Yasmin Castro <yasminsdcastro@gmail.com>
2023-10-02 11:38:23 -0300

    feat: update nxt

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,10 @@ if (!process.env.MONGO_URL) {
   throw new Error("MONGO URL NOT DEFINED");
 }
 
+if (!process.env.MONGO_URL_GLOBAL) {
+  throw new Error("MONGO_URL_GLOBAL NOT DEFINED");
+}
+
 if (!process.env.DB_NAME) {
   throw new Error("DB_NAME NOT DEFINED");
 }
@@ -15,6 +19,7 @@ if (!process.env.GUESTS_PASSWORD) {
 }
 
 export const MONGO_URL = process.env.MONGO_URL;
+export const MONGO_URL_GLOBAL = process.env.MONGO_URL_GLOBAL;
 export const DB_NAME = process.env.DB_NAME;
 export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 export const SECRET_TOKEN = process.env.SECRET_TOKEN;
