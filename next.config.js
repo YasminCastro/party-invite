b6a31bcd29ecb7343a523e2a0c9de251fda1dd11
Yasmin Castro <yasminsdcastro@gmail.com>
2023-10-05 14:21:50 -0300

    v2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    MONGO_URL: process.env.MONGO_URL,
-    DB_NAME: process.env.DB_NAME,
-    SECRET_TOKEN: process.env.SECRET_TOKEN,
-    GUESTS_PASSWORD: process.env.GUESTS_PASSWORD,
-    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
