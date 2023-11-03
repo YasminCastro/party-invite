c56ba59b6554f175942b19b39bfbae849a39af45
Yasmin Castro <yasminsdcastro@gmail.com>
2023-11-03 14:21:26 -0300

    refactor; mobile

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
 
   return (
-    <div className="min-h-screen bg-home bg-cover bg-center">
+    <div className="min-h-screen bg-home bg-cover bg-center flex items-center justify-center">
       <div>
         {isAdmin && (
           <Link href="/admin">
@@ -37,32 +37,32 @@ export default function Home() {
         </Button>
       </div>
 
-      <div className="absolute top-1/3 w-full max-md:top-1/4">
-        <div className="flex justify-evenly max-md:flex-col max-md:items-center max-md:gap-y-4">
-          <h1 className="font-title text-6xl text-login-title max-sm:text-5xl font-bold">
+      <div className="w-full flex justify-evenly max-md:flex-col max-md:items-center max-md:gap-y-4">
+        <div className="w-1/3 max-xl:w-1/2 max-md:mb-4 max-md:w-3/4">
+          <h1 className="font-title text-center text-login-title font-bold text-7xl max-xl:text-5xl max-sm:text-4xl">
             {projectConfig.partyName}
           </h1>
+        </div>
 
-          <div className="flex w-1/3 flex-col items-center gap-2 max-md:w-2/3 max-sm:w-11/12">
-            <h3 className="font-title text-3xl text-login-title max-sm:text-2xl">
-              {projectConfig.data}
-            </h3>
+        <div className="flex w-1/3 flex-col items-center gap-2 max-md:w-2/3 max-sm:w-11/12">
+          <h3 className="font-title text-3xl text-login-title max-lg:text-2xl max-sm:text-xl">
+            {projectConfig.data}
+          </h3>
 
-            <Link href="/confirm-presence">
-              <Button
-                className="w-full"
-                color={projectConfig.buttonColor}
-                onClick={() => {
-                  setLoading(true);
-                }}
-                disabled={loading}
-              >
-                {loading ? "Carregando..." : "Confirmar presença"}
-              </Button>
-            </Link>
+          <Link href="/confirm-presence">
+            <Button
+              className="w-full"
+              color={projectConfig.buttonColor}
+              onClick={() => {
+                setLoading(true);
+              }}
+              disabled={loading}
+            >
+              {loading ? "Carregando..." : "Confirmar presença"}
+            </Button>
+          </Link>
 
-            <AddressMap />
-          </div>
+          <AddressMap />
         </div>
       </div>
     </div>
