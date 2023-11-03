a6094ad4e2115f6a657322b949ed2a1b2b6a7ee1
Yasmin Castro <yasminsdcastro@gmail.com>
2023-11-03 14:03:37 -0300

    refacto: bg

--- app/convidades/page.tsx
+++ app/convidades/page.tsx
@@ -4,7 +4,7 @@ import ListGuests from "@/components/AdminCards/ListGuests";
 
 export default function Convidades() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-home">
+    <div className="flex min-h-screen items-center justify-center bg-home bg-cover bg-center">
       <div className="absolute top-6 flex gap-4 max-md:left-6 max-md:gap-2 max-sm:left-4"></div>
       <ListGuests isAdminPage={false} />
     </div>
