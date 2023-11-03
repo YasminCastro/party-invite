57f6fce044a333c5c6245a0da591aac27db94734
Yasmin Castro <yasminsdcastro@gmail.com>
2023-11-03 14:10:39 -0300

    fix: title

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,9 +5,9 @@ import projectConfig from "@/config/project";
 
 export default function Login() {
   return (
-    <div className="min-h-screen bg-login bg-cover bg-center">
-      <div className="flex flex-col items-center justify-center gap-20 py-16">
-        <h1 className="whitespace-nowrap font-title text-7xl text-login-title font-bold">
+    <div className="min-h-screen bg-login bg-cover bg-center flex items-center justify-center">
+      <div className="flex flex-col items-center gap-20 py-16">
+        <h1 className="whitespace-nowrap font-title text-7xl text-login-title font-bold max-lg:text-6xl  max-md:text-5xl max-sm:whitespace-normal max-sm:text-center max-sm:text-4xl">
           {projectConfig.partyName}
         </h1>
         <LoginForm />
