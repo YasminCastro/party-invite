fee23b2fd051cca942a99ebf0866c7b2153304da
Yasmin Castro <yasminsdcastro@gmail.com>
2023-12-05 17:48:46 -0300

    feat: reload when edit

--- components/AdminCards/ListGuests/GuestTable.tsx
+++ components/AdminCards/ListGuests/GuestTable.tsx
@@ -214,6 +214,7 @@ export default function GuestTable({
             setOpenModal(modal);
           }}
           guest={selectedGuest}
+          setReloadGuests={setReloadGuests}
         />
       )}
     </Flowbite>
