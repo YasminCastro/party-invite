fbb86f886add94cd96c13e274eda815875f7c11b
Yasmin Castro <yasminsdcastro@gmail.com>
2023-12-05 19:02:14 -0300

    refactor:> next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Isso se aplica a todas as páginas
+        headers: [
+          { key: "Cache-Control", value: "no-store" }, // Desativa o cache
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
