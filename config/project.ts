6493f5162251769dffcf2e6350be31dba1ee0a4b
Yasmin Castro <yasminsdcastro@gmail.com>
2024-04-22 20:33:40 -0300

    feat: first ajusts

--- config/project.ts
+++ config/project.ts
@@ -14,17 +14,17 @@ interface IProject {
 }
 
 const projectConfig: IProject = {
-  partyName: "Glaucia Eletrohits 30",
-  seoName: "Glaucia Eletrohits",
-  data: "16/12/2023 às 19h",
-  lastDateToConfirm: "13/12",
+  partyName: "Quintal do Gê",
+  seoName: "Quintal do Gê",
+  data: "22/06/2024 às 11h30",
+  lastDateToConfirm: "19/12",
   spotifyUrl: null,
-  whatsappNumber: "5562983321120",
-  addressWritten: "Rua 37, QD 17, LT 07 - Jardim Bela Vista",
+  whatsappNumber: "5562984016328",
+  addressWritten: "Live Tower Lozandes - Park Lozandes",
   address:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.825514907909!2d-49.241651922436304!3d-16.735555705669753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef064c309f58f%3A0xa4e43a686b274059!2sR.%2037%2C%20623-401%20-%20Jardim%20Bela%20Vista%2C%20Aparecida%20de%20Goi%C3%A2nia%20-%20GO%2C%2074912-090!5e0!3m2!1spt-BR!2sbr!4v1695839230162!5m2!1spt-BR!2sbr",
+    "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4544.630192546737!2d-49.224643498822296!3d-16.69872562096442!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef1b64b07dd33%3A0x89d093940492476a!2sLive%20Tower%20Lozandes!5e0!3m2!1spt-BR!2sbr!4v1713828697446!5m2!1spt-BR!2sbr",
   buttonColor: "light",
-  chavePix: "3abb357e-b1bb-43f1-98d7-a05be29f4bf8",
+  chavePix: "",
 };
 
 export default projectConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,80 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      fontFamily: {
-        title: "var(--font-orbitron)",
-        text: "var(--font-roboto)",
-        alt: "var(--font-bailjamjuree)",
-      },
-      backgroundImage: {
-        login: "url('/login/background.png')",
-        home: "url('/home/background.png')",
-      },
-
       colors: {
-        login: {
-          title: "#f8fafc",
-          form: "color: rgb(0 0 0 / var(--tw-text-opacity))",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        admin: {
-          title: "#f8fafc",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
