5f822afbd44068970ce0ca0878c7ebb9e6d9041b
Yasmin Castro <yasminsdcastro@gmail.com>
2024-04-26 11:33:57 -0300

    feat: icons

--- components/Global/GuestList/GuestTable/Index.tsx
+++ components/Global/GuestList/GuestTable/Index.tsx
@@ -8,6 +8,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { AiFillCheckCircle, AiFillCloseCircle } from "react-icons/ai";
+import { Checkbox } from "@/components/ui/checkbox";
 
 interface IProps {
   isAdmin: boolean;
@@ -31,8 +33,16 @@ export default function GuestTable({ isAdmin, guests }: IProps) {
         {guests.map((guest) => (
           <TableRow>
             <TableCell>{guest.name}</TableCell>
-            <TableCell>{guest.status}</TableCell>
-            <TableCell>{guest.receivedInvitation}</TableCell>
+            <TableCell>
+              {guest.status ? (
+                <AiFillCheckCircle className="text-lg text-green-500" />
+              ) : (
+                <AiFillCloseCircle className="text-lg text-red-500" />
+              )}
+            </TableCell>
+            <TableCell>
+              <Checkbox checked={guest.receivedInvitation} />
+            </TableCell>
             {isAdmin && <TableCell>Editar</TableCell>}
             {isAdmin && <TableCell>Excluir</TableCell>}
           </TableRow>
