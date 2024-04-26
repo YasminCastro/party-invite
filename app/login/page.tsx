8e1bdf1a582f2136787348623b6f6ae912b688c7
Yasmin Castro <yasminsdcastro@gmail.com>
2024-04-26 10:17:19 -0300

    prettier

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,9 +3,9 @@ import projectConfig from "@/config/project";
 
 export default function Login() {
   return (
-    <div className="min-h-screen bg-login bg-cover bg-center flex items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center bg-login bg-cover bg-center">
       <div className="flex flex-col items-center gap-20 py-16">
-        <h1 className="whitespace-nowrap font-title text-primary text-7xl max-lg:text-6xl  max-md:text-5xl max-sm:whitespace-normal max-sm:text-center max-sm:text-4xl">
+        <h1 className="whitespace-nowrap font-title text-7xl text-primary max-lg:text-6xl  max-md:text-5xl max-sm:whitespace-normal max-sm:text-center max-sm:text-4xl">
           {projectConfig.partyName}
         </h1>
         <LoginForm />
