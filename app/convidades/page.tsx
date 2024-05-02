dcd585469b4c2553038ef4dae23ae7ec7b83e439
Yasmin Castro <yasminsdcastro@gmail.com>
2024-05-02 11:48:47 -0300

    feat: convidades

--- app/convidades/page.tsx
+++ app/convidades/page.tsx
@@ -1,19 +1,17 @@
 "use client";
 
-import ListGuests from "@/components/AdminCards/ListGuests";
-import GoBackButton from "@/components/Global/GoBackButton/Index";
+import GuestList from "@/components/Global/GuestList/Index";
 import { useState } from "react";
 
 export default function Convidades() {
-  const [reloadGuests, setReloadGuests] = useState<string>("");
+  const [refreshList, setRefreshList] = useState("");
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-login bg-cover bg-center">
-      <GoBackButton title="Início" path="/" />
-      <ListGuests
-        isAdminPage={false}
-        setReloadGuests={setReloadGuests}
-        reloadGuests={reloadGuests}
+    <div>
+      <GuestList
+        isAdmin={false}
+        refreshList={refreshList}
+        setRefreshList={setRefreshList}
       />
     </div>
   );
