3f70559f54901b9689cd128718e18d9058d5b812
Yasmin <yasminsdcastro@gmail.com>
2024-06-27 10:28:18 -0300

    refactor: change chave pix

--- config/project.ts
+++ config/project.ts
@@ -20,7 +20,7 @@ const projectConfig: IProject = {
   addressWritten: "Quintal da árvore - Jardim Planalto",
   address:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23573.820360106623!2d-49.30941839019124!3d-16.705237530056333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935ef74366cd9a6d%3A0xb6b822483395a821!2sQuintal%20da%20%C3%81rvore!5e0!3m2!1spt-BR!2sbr!4v1717524798696!5m2!1spt-BR!2sbr",
-  chavePix: "786dc37e-4ba0-4c64-a1ca-c172a2dd327e",
+  chavePix: "373d7e9c-5cb4-427e-99a0-1a7898d59f9b",
   spotifyUrl:
     "https://open.spotify.com/playlist/7rppPNJzZPFPCaRzojzoAu?si=d9c80b27bc024b9b",
 };
